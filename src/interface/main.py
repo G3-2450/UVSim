d525c91f54cf0f8e233ba308fb7b7f19c1e9ac38
toldham4 <taylor.oldham4@gmail.com>
2025-06-29 16:22:09 -0600

    Completed Memory/Registers pane and added sample memory to main to test scrollability

--- src/interface/main.py
+++ src/interface/main.py
@@ -17,7 +17,46 @@ class LeftPaneWidget(BoxLayout):
     pass
 
 class MemRegWidget(BoxLayout):
-    pass
+    # Sample 100 memory slots to test scrollability 
+    def on_kv_post(self, base_widget):
+        self.populate_memory()
+
+    def populate_memory(self):
+        memory_box = self.ids.memory_box
+        for i in range(100):
+            row = BoxLayout(
+                orientation = 'horizontal',
+                size_hint_y = None,
+                height = dp(40),
+                spacing = dp(8)
+            )
+
+            mem_lable = Label(
+                text=f"{i:02}",
+                size_hint_x=None,
+                width=dp(30),
+                halign='right',
+                valign='middle',
+                text_size=(dp(30), dp(40)),
+                color=(1, 1, 1, 1),
+                font_size='16sp'
+            )
+
+            mem_input = TextInput(
+                text='+0000',
+                readonly=True,
+                multiline=False,
+                font_size='18sp',
+                halign='center',
+                background_color=(0, 0, 0, 1),
+                foreground_color=(1, 1, 1, 1),
+                size_hint_x=1,
+                height=dp(30)
+            )
+
+            row.add_widget(mem_lable)
+            row.add_widget(mem_input)
+            memory_box.add_widget(row)
 
 class ConsoleWidget(BoxLayout):
     pass
